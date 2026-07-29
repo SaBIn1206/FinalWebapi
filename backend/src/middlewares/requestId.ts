4b8d5d29b3aabb5f103c8c86b48e92a08f810581
SaBIn1206 <sabinshrestha0624@gmail.com>
2026-07-29 10:42:59 +0545

    Add backend: backend/src/middlewares/requestId.ts

---
+++ backend/src/middlewares/requestId.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from 'express';
+import { randomUUID } from 'crypto';
+
+export const requestIdMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  const requestId = randomUUID();
+  req.headers['x-request-id'] = requestId;
+  res.setHeader('X-Request-Id', requestId);
+  next();
+};
